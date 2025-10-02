21a16bcc918392dfdc3b53f87816dcf8ad5e0a75
Afonso Carvalho <afonsomiguelcarvalho2006@gmail.com>
2025-10-02 02:34:46 +0100

    Added script to handle transactions (with some validation)

--- script.js
+++ script.js
@@ -16,3 +16,37 @@ window.addEventListener('DOMContentLoaded', init);
 function saveTransactions() {
     localStorage.setItem('transactions', JSON.stringify(transactions));
 }
+
+
+
+
+// Below is the script for adding a transaction
+
+document.getElementById('transactionForm').addEventListener('submit', function(ev) {
+    ev.preventDefault();
+
+    const description = document.getElementById('description').value.trim();
+    const amount = document.getElementById('amount').value.trim();
+    const type = document.getElementById('type').value.trim();
+    const category = document.getElementById('category').value.trim();
+    const date = document.getElementById('date').value.trim();
+
+    if (amount <= 0 || isNaN(amount)) {
+        alert('You must enter a valid amount');
+        return;
+    }
+
+    const transaction = {
+        id: Date.now(),
+        description,
+        amount,
+        type,
+        category,
+        date
+    };
+
+    transactions.push(transaction);
+
+    this.reset();
+    document.getElementById(date).valueAsDate = new Date();
+});
